829f6f6cd3f20dd745a5a78cb5b497499cc09293
FedeJack06 <giacobbefederico6@gmail.com>
2023-11-23 14:58:08 +0100

    up fede

--- 7hoFit/fitlin.C
+++ 7hoFit/fitlin.C
@@ -3,12 +3,13 @@ namespace data{
 }
 
 double fun(const double *x,const double *par){
-  return par[0]*(*x)+par[1];
+  return par[1]*(*x)+par[0];
 }
 
 void fcn(int &npar,double *gin,double &f,double *par,int iflag){
   f = 0.0;
   for (int i=0;i<data::x.size();i++){
+    f += pow(data::y[i]-fun(&data::x[i],par),2)/(pow(data::ey[i],2) + pow(par[1]*data::ex[i],2));
     // Calcolo del Chi2
   }
 }
@@ -21,16 +22,22 @@ void fitlin(){
   }
 
   // Define the minimization problem
-  // TMinuit *minuit = new TMinuit(...); // numero di parametri  
-  //  minuit->SetFCN(fcn);
-  // minuit->DefineParameter(indice,"nome",valin,step,min,max);
+  TMinuit *minuit = new TMinuit(2); // numero di parametri  
+  minuit->SetFCN(fcn);
+  //minuit->SetErrorDef(1.0);
+  minuit->DefineParameter(0,"beta_0",0,0.01,0.0,0.0);
+  minuit->DefineParameter(1,"beta_1",4,0.01,0.0,0.0);
   // per ogni indice inserisco nome, valore iniziale, step, minimo, massimo del parametro
 
   // Minimize
-  //  minuit->Command("MIGRAD"); // Comando di minimizzazione
+  minuit->Command("MIGRAD"); // Comando di minimizzazione //HESSE calcolo preciso matrice di covarianza
+  //MINOS calcolo erorrori asim
+  //trova solo il minimo, e i parametri, non disegna
+  double beta0, beta1, ebeta0, ebeta1;
 
   // Get result
-  // minuit->GetParameter(indice,val,eval);
+  minuit->GetParameter(0,beta0,ebeta0);
+  minuit->GetParameter(1,beta1,ebeta1);
   // per ogni indice estraggo il valore del parametro e del suo errore
   // minuit->GetParameter(indice,val,eval);
 
