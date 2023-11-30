150fe8c006c9a6a6c5f810f208eb300b5a7cb3ce
FedeJack06 <giacobbefederico6@gmail.com>
2023-11-30 15:56:56 +0100

    up

--- 8hoFit/fitexpbin.py
+++ 8hoFit/fitexpbin.py
@@ -6,6 +6,12 @@ from   math    import *
 def flogl(tau):
     val = 0
     # Definisco logl
+    for i in range(1, h.GetNbinsX()+1):
+        ni = h.GetBinContent(i)
+        timin = h.GetBinLowEdge(i)
+        timax = timin + h.GetBinWidth(i)
+        pi = exp(-timin/tau) -exp(-timax/tau)
+        val = val - ni*log(pi)
     return val
 
 #Main
@@ -15,13 +21,18 @@ for line in open("exp.dat"):
 
 m = Minuit(flogl,tau=2)
 # Istruisco fir di logl
-# m.migrad()       
+m.errordef = 0.5
+m.print_level = 3
+m.migrad()       
 
-# tau = m.values[0]
+tau = m.values[0]
 h.Draw("E")
 
 # Disegno del fit
 f = TF1("f","[0]*1/[1]*exp(-x/[1])",0,20)
+f.SetParameter(0,h.GetEntries()*h.GetBinWidth(1))
+f.SetParameter(1,tau)
+f.Draw("SAME")
 
 gApplication.Run(True)
 
